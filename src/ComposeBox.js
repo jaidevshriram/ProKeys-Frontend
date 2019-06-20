652005ed8778ef8a2dd3a41ac49fe8a1063322a2
JAIDEV123 <jaidevtrumpet@gmail.com>
2019-06-20 22:45:02 +0530

    Added font awesome to composebox

--- src/ComposeBox.js
+++ src/ComposeBox.js
@@ -1,6 +1,10 @@
 /* eslint-disable no-unused-vars */
 import React from "react";
 
+// Font Awesome
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+
 export default class Compose extends React.Component {
     constructor(props) {
         super(props);
