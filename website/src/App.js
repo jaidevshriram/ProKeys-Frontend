acbb1a7e32bef798339465721fdfea8b1d4920ae
JAIDEV123 <jaidevtrumpet@gmail.com>
2019-07-08 02:09:19 +0530

    Routing done:

--- website/src/App.js
+++ website/src/App.js
@@ -1,14 +1,28 @@
 import React from 'react';
 import Navbar from './Navbar';
 import Hero from './Hero';
+import GettingStarted from './GettingStarted';
+ 
+export default class App extends React.Component {
 
-function App() {
-  return (
-    <React.Fragment>
-      <Navbar />
-      <Hero />
-    </React.Fragment>
-  );
+  render() {
+
+    var component;
+
+    if(window.location.pathname == '/' || window.location.pathname == '/index' ) {
+      component = <Hero/>;
+    }
+    else if (window.location.pathname === '/getting-started') {
+      component = <GettingStarted />;
+    }
+
+
+    return (
+      <React.Fragment>
+        <Navbar />
+        {component}
+      </React.Fragment>
+    );
+  }
+
 }
-
-export default App;
