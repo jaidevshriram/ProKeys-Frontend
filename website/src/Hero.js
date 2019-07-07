aadb43c3bf888277f2071253586598b862c60146
JAIDEV123 <jaidevtrumpet@gmail.com>
2019-07-08 00:49:39 +0530

    Hero started

---
+++ website/src/Hero.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+export default class Hero extends React.Component {
+	render() {
+		return (
+			<div class="container-fluid h-100 bg-primary">
+				<div class="row h-100">
+					<div class="col-12 col-md-6 align-self-center">
+						<div class="my-auto h-100 text-center px-5">
+							<h1>The Best Tool for Typists</h1>
+							<h3>ProKeys is a text expander that helps you focus on your content, not on the actual content.</h3>
+						</div>
+					</div>
+					<div class="col-12 col-md-6">
+					</div>
+				</div>
+			</div>
+		);
+	}
+}
