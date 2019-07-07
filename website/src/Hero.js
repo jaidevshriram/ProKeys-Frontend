bc28bec2d8f918b72e0b9979fd69c3b5fe67a8f3
JAIDEV123 <jaidevtrumpet@gmail.com>
2019-07-08 01:11:20 +0530

    Basic homepage done

--- website/src/Hero.js
+++ website/src/Hero.js
@@ -1,17 +1,19 @@
 import React from 'react';
+import mockup from './img/mockup.png';
 
 export default class Hero extends React.Component {
 	render() {
 		return (
-			<div class="container-fluid h-100 bg-primary">
-				<div class="row h-100">
-					<div class="col-12 col-md-6 align-self-center">
-						<div class="my-auto h-100 text-center px-5">
+			<div className="container-fluid h-md-                                                                                                                                                                                  95 bg-navy">
+				<div className="row h-100">
+					<div className="col-12 col-md-6 align-self-center">
+						<div className="my-auto h-100 text-center text-white px-5">
 							<h1>The Best Tool for Typists</h1>
 							<h3>ProKeys is a text expander that helps you focus on your content, not on the actual content.</h3>
 						</div>
 					</div>
-					<div class="col-12 col-md-6">
+					<div className="col-12 col-md-6 align-self-center">
+						<img src={mockup} className="img-fluid"/>
 					</div>
 				</div>
 			</div>
