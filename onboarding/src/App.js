4257c3d69a367fc114e030ab87cd0af761b01d90
JAIDEV123 <jaidevtrumpet@gmail.com>
2019-07-11 01:16:54 +0530

    Started out on intro

--- onboarding/src/App.js
+++ onboarding/src/App.js
@@ -1,26 +1,63 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Intro from './Intro';
+
+export default class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      progress: 0,
+    }
+  }
+
+  next() {
+    this.setState({
+      progress: this.state.progress + 1,
+    })
+  }
+
+  previous() {
+    this.setState({
+      progress: this.setState.progress - 1,
+    })
+  }
+
+  render() {
+    return (
+      <React.Fragment>
+        <div className="container-fluid h-100">
+          <div className="row h-100">
+            <div className="col">
+
+              <div className="d-inline-flex justify-content-center w-100 py-5">
+                <h1 className="font-weight-bold">Welcome to ProKeys!</h1>
+              </div>
+
+              <div className="container h-75">
+                <div className="row h-100">
+                  <div className="col">
+                    <div className="light-outer-box h-100">
+                      <div className="light-inner-box p-4">
+                        <div className="d-flex h-100 w-100 align-items-center justify-content-center">
+                          <div className="container-fluid">
+                            <div className="row">
+                              <div className="col text-center">
+                                <Intro />
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+
+            </div>
+          </div>
+        </div>
+      </React.Fragment>
+    );
+  }
 }
-
-export default App;
---
+++ onboarding/src/Intro.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default class Intro extends React.Component {
+	render() {
+		return (
+			<React.Fragment>
+		        <h2>Thanks for downloading the extension!</h2>
+		        <h5>Use this brief tutorial to become a power user in no time!</h5>
+	        </React.Fragment>
+		);
+	}
+}
