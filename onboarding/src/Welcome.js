98868aaf25290c6683abf5dee16017da9d3e2354
GaurangTandon <1gaurangtandon@gmail.com>
2019-07-15 15:43:40 +0530

    give actual effect of expansion

--- onboarding/src/Welcome.js
+++ onboarding/src/Welcome.js
@@ -1,37 +1,43 @@
 import React from 'react';
-import Typed from 'typed.js';
+import typer from "./typer";
 
 export default class Welcome extends React.Component {
 	componentDidMount() {
 		const options = {
-		  strings: ["ProKeys Text Expander!"],
-		  typeSpeed: 60,
+			string: "Welcome to pk tx exp !",
+			typeSpeed: 100,
+			map: {
+				"pk ": "ProKeys ",
+				"tx ": "text ",
+				"exp ": "expander"
+			}
 		}
 
-		this.typed = new Typed(this.el, options);
+		this.typed = new typer(this.el, options);
 	}
+
 	render() {
 		return (
 			<React.Fragment>
-		        <div className="container-fluid h-75 pt-5">
-		        	<div className="row h-100">
-		        		<div className="col d-flex align-self-center justify-content-center">
-		        			<div className="container-fluid ">
-		        				<div className="row ">
-		        					<div className="col">
-									
-										<h1 className="font-weight-bold">Welcome to <span className="welcome-title" ref={(el) => {this.el = el; }} /></h1>
+				<div className="container-fluid h-75 pt-5">
+					<div className="row h-100">
+						<div className="col d-flex align-self-center justify-content-center">
+							<div className="container-fluid ">
+								<div className="row ">
+									<div className="col">
+
+										<h1 className="font-weight-bold"><span className="welcome-title" ref={(el) => { this.el = el; }} /></h1>
 										<p className="h4 pb-5 text-muted"><i>The ultimate typing utility for all your needs!</i></p>
 
 										<p className="h5">This is a quick start guide to make you a pro user in just 2 minutes!</p>
 										<p className="h4 font-weight-bold pt-4">Snippets are represented as <mark>Snippet</mark></p>
-		        					</div>
-		        				</div>
-		        			</div>
-		        		</div>
-		        	</div>
-		        </div>
-	        </React.Fragment>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</React.Fragment>
 		);
 	}
 }
