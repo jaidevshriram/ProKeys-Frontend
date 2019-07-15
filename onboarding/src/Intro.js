88083580560de3fc012c5c96429db9e19442a9f0
GaurangTandon <1gaurangtandon@gmail.com>
2019-07-15 17:17:16 +0530

    add expand functionality

--- onboarding/src/Intro.js
+++ onboarding/src/Intro.js
@@ -1,113 +1,81 @@
 import React from 'react';
 import ProgressBar from './ProgressBar';
-import KeyMap from './KeyMap';
 
-// BEGIN TEST AREA
-let activeKeys = [],
-    sequence = [],
-    pressedKeyCount = 0,
-    sequenceClearInterval,
-    map = new KeyMap();
-
-const main = () => {
-    map.bind(["Tab"], () => { console.log("THIS SHOULD EXPAND SOME STUFF") })
-    document.body.addEventListener("keydown", register);
-    document.body.addEventListener("keyup", unRegister);
-}
-
-const register = e => {
-    let key = e.key;
-
-    e.stopPropagation();
-
-    if(activeKeys.indexOf(key) < 0) {
-        activeKeys.push(key);
-        pressedKeyCount++;
-    }
-
-    clearTimeout(sequenceClearInterval);
-    sequenceClearInterval = setTimeout(() => {
-        console.log("exec", sequence);
-
-        let bind = map.getBind(sequence);
-        if(bind)
-            bind();
-
-        sequence = [];
-        activeKeys = [];
-    }, 500);
-
-    console.log(activeKeys, pressedKeyCount);
-}
-
-const unRegister = e => {
-    pressedKeyCount--;
-
-    if(pressedKeyCount === 0) {
-        sequence.push(activeKeys.join('+'));
-        activeKeys = [];
-    }
-}
-
-console.log(activeKeys, pressedKeyCount);
-
-main();
-// END TEST AREA
 
 export default class Intro extends React.Component {
+	/**
+	 * @param {Event} e
+	 */
+	onkeydown(e) {
+		const node = e.target;
+		// replacer logic here
+		if (node && /snippet-input/.test(node.className) && e.key === "Tab") {
+			const nss = node.selectionStart;
+			node.value = node.value.replace(/demo/i, "Your snippet worked!");
+			node.selectionEnd = node.selectionStart = nss + "Your snippet worked!".length - "demo".length;
+			e.preventDefault();
+			e.stopPropagation();
+			return false;
+		}
+	}
+
+	componentDidMount() {
+		document.body.addEventListener("keydown", this.onkeydown);
+	}
+
 	render() {
 		return (
 			<React.Fragment>
-	        
+
 				<ProgressBar {...this.props} />
 
-		        <div className="container-fluid h-75 pt-5">
-		        	<div className="row h-100">
-		        		<div className="col-5 text-left">
+				<div className="container-fluid h-75 pt-5">
+					<div className="row h-100">
+						<div className="col-5 text-left">
 							<div className="card text-center h-100">
-							  <div className="card-header">
-							    <span className="h5 font-weight-bold">1.&nbsp;&nbsp;</span> This is your abbreviation or <span className="font-weight-bold">Snippet</span>
-							  </div>
-							  <div className="card-body d-flex align-items-center">
-							  	<div>
-							    <h5 className="card-title">Snippet Name: <span className="font-weight-bold"><mark>Demo</mark></span></h5>
-							    <p className="card-text pt-4"><i>Snippet names expand into the text you want it to!</i></p>
-							  	</div>
-							  </div>
-							  <div className="card-footer text-muted">
-							    Snippet names are case insensitive!
+								<div className="card-header">
+									<span className="h5 font-weight-bold">1.&nbsp;&nbsp;</span> This is your abbreviation or <span className="font-weight-bold">Snippet</span>
+								</div>
+								<div className="card-body d-flex align-items-center">
+									<div>
+										<h5 className="card-title">Snippet Name: <span className="font-weight-bold"><mark>Demo</mark></span></h5>
+										<p className="card-text pt-4"><i>Snippet names expand into the text you want it to!</i></p>
+									</div>
+								</div>
+								<div className="card-footer text-muted">
+									Snippet names are case insensitive!
 							  </div>
 							</div>
-		        		</div>
-		        		<div className="col-2 d-flex align-items-start flex-column bg-light">
-		        			<span className="h5 font-weight-bold">2.&nbsp;&nbsp;</span> 
-		        			<span className="my-auto">
-		        				Type the snippet and press the default hotkey:<br/><br/>
-		        				<kbd className="h3">Tab</kbd>
-		        			</span>
-		        		</div>
-		        		<div className="col-5 text-left">
+						</div>
+						<div className="col-2 d-flex align-items-start flex-column bg-light">
+							<span className="h5 font-weight-bold">2.&nbsp;&nbsp;</span>
+							<span className="my-auto">
+								Type the snippet and press the default hotkey:<br /><br />
+								<kbd className="h3">Tab</kbd>
+							</span>
+						</div>
+						<div className="col-5 text-left">
 							<div className="card text-center h-100">
-							  <div className="card-header">
-							    <span className="h5 font-weight-bold">3.&nbsp;&nbsp;</span> You now expand your snippet!
+								<div className="card-header">
+									<span className="h5 font-weight-bold">3.&nbsp;&nbsp;</span> You now expand your snippet!
 							  </div>
-							  <div className="card-body align-items-center">
-							  	Type <mark>Demo</mark> and hit <kbd>Tab</kbd> in the box to below to see the magic happen!
+								<div className="card-body align-items-center">
+									Type <mark>Demo</mark> and hit <kbd>Tab</kbd> in the box to below to see the magic happen!
 							  	<form className="pt-5">
-								  	<div className="form-group">
-								    	<textarea className="form-control" rows="5">
-								    	</textarea>
-								  	</div>
-							  	</form>
-							  </div>
-							  <div className="card-footer text-muted">
-							    You can change the Tab key to any combination in <i>Settings</i>
-							  </div>
+										<div className="form-group">
+											<textarea className="form-control snippet-input" rows="5">
+											</textarea>
+										</div>
+									</form>
+								</div>
+								<div className="card-footer text-muted">
+									You can change the Tab key to any combination in <i>Settings</i>
+								</div>
 							</div>
-		        		</div>
-		        	</div>
-		        </div>
-	        </React.Fragment>
+						</div>
+					</div>
+				</div>
+			</React.Fragment >
 		);
 	}
 }
