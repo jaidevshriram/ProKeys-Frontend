db0d3dad721ad7cc1efa9da37c94ce11833943f0
JAIDEV123 <jaidevtrumpet@gmail.com>
2019-07-18 23:40:45 +0530

    background work done

--- src/BaseLayout.js
+++ src/BaseLayout.js
@@ -8,6 +8,46 @@ import FolderRender from "./ManageSnippet";
 import Settings from "./Settings";
 import Compose from "./Compose";
 
+// export class BaseLayout extends React.Component {
+//     newCompose() {
+//         this.createCompose();
+//     }
+
+//     linkToNewSnippet(realCreateCompose) {
+//         this.createCompose = realCreateCompose;
+//     }
+
+//     render() {
+//         return (
+//             <React.Fragment>
+//                 <Options />
+
+//                 <Route exact path="/" render={props => (
+//                     <SnippetOptions {...props} onclick={this.newCompose.bind(this)} />
+//                 )}
+//                 />
+
+//                 <Route exact path="/Snippet" component={SnippetOptions} />
+
+//                 <Switch>
+//                     <Route exact path="/"
+//                         render={routeProps => (
+//                             <FolderRender {...routeProps} folder="Snippets" />
+//                         )} />
+//                     <Route path="/Snippet" render={routeProps => (
+//                         <FolderRender {...routeProps} folder="Snippets" />
+//                     )} />
+//                     <Route path="/Setting" component={Settings} />
+//                 </Switch>
+
+//                 <Compose linkToNewSnippet={this.linkToNewSnippet.bind(this)} />
+
+//             </React.Fragment>
+//         );
+//     }
+// }
+
+
 export class BaseLayout extends React.Component {
     newCompose() {
         this.createCompose();
@@ -20,28 +60,21 @@ export class BaseLayout extends React.Component {
     render() {
         return (
             <React.Fragment>
-                <Options />
-
-                <Route exact path="/" render={props => (
-                    <SnippetOptions {...props} onclick={this.newCompose.bind(this)} />
-                )}
-                />
-
-                <Route exact path="/Snippet" component={SnippetOptions} />
-
-                <Switch>
-                    <Route exact path="/"
-                        render={routeProps => (
-                            <FolderRender {...routeProps} folder="Snippets" />
-                        )} />
-                    <Route path="/Snippet" render={routeProps => (
-                        <FolderRender {...routeProps} folder="Snippets" />
-                    )} />
-                    <Route path="/Setting" component={Settings} />
-                </Switch>
-
-                <Compose linkToNewSnippet={this.linkToNewSnippet.bind(this)} />
-
+                <div className="container-fluid h-100">
+                    <div className="row h-100">
+                        <div className="col-3 light-sidebar-background">
+                            <div className="container-fluid h-25">
+                                <div className="row h-100">
+                                    <div className="col d-flex align-items-center justify-content-center">
+                                        <span className="display-3 text-white">proKeys</span>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="col-9 light-outer-box">
+                        </div>
+                    </div>
+                </div>
             </React.Fragment>
         );
     }
