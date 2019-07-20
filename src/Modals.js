d67c8fa42a240e9b2f4615a774e3c14a091cc4e2
GaurangTandon <1gaurangtandon@gmail.com>
2019-07-20 13:09:28 +0530

    fix quotations

--- src/Modals.js
+++ src/Modals.js
@@ -1,20 +1,20 @@
-import React from 'react';
+import React from "react";
 
-//Custom Imports
-import TooManyCompose from './Modals/TooManyCompose';
-import NewFolder from './Modals/NewFolder';
-import Export from './Modals/Export';
-import Import from './Modals/Import';
+// Custom Imports
+import TooManyCompose from "./Modals/TooManyCompose";
+import NewFolder from "./Modals/NewFolder";
+import Export from "./Modals/Export";
+import Import from "./Modals/Import";
 
 export default class Modals extends React.Component {
-	render() {
-		return (
-			<React.Fragment>
-				<TooManyCompose />
-				<NewFolder />
-				<Export />
-				<Import />
-			</React.Fragment>
-		)
-	}
+    render() {
+        return (
+            <React.Fragment>
+                <TooManyCompose />
+                <NewFolder />
+                <Export />
+                <Import />
+            </React.Fragment>
+        );
+    }
 }
